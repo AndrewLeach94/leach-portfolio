4ed030753c55f40e5425be277cab7b2cf04229c7
AndrewLeach94 <andrew_leach@outlook.com>
2024-10-26 22:43:09 -0500

    thumbnail styling work

--- src/components/case-study-thumbnail/CaseStudy.tsx
+++ src/components/case-study-thumbnail/CaseStudy.tsx
@@ -7,26 +7,51 @@ interface caseStudyProps {
 
 const CaseStudy = ({title, assetSrc} : caseStudyProps) => {
     const detailsModal = useRef<HTMLDialogElement>(null);
-    // Function to open the modal
+    
     const openModal = () => {
-        console.log('open modal triggered');
         detailsModal.current?.showModal();
     };
-    // Function to close the modal
-    // const closeModal = () => {
-    //     // detailsModal.current?.close();
-    // };
+    const closeModal = () => {
+        console.log('close modal triggered');
+        detailsModal.current?.close();
+    };
+
+    const thumbnailVideoRef = useRef<HTMLVideoElement>(null);
+    
+    const handleMouseEnter = () => {
+        thumbnailVideoRef.current?.play();
+    };
+    
+    const handleMouseLeave = () => {
+        thumbnailVideoRef.current?.pause();
+    };
+
     return (
         <div className="case-study-thumbnail">
-            <button onClick={() => openModal()}>
-                <p className="title">{title}</p>
-                <video muted>
+            <button className="thumbnail-container" onClick={() => openModal()}>
+                <div className="title">
+                    <p>{title}</p>
+                </div>
+                <div className="expand-icon">+</div>
+                <video
+                ref={thumbnailVideoRef}
+                muted
+                loop
+                onMouseEnter={() => handleMouseEnter()}
+                onMouseLeave={() => handleMouseLeave()}
+                >
                     <source src={assetSrc} type="video/webm" />
                 </video>
             </button>
              <dialog ref={detailsModal}>
-                <h2>Native Modal Dialog</h2>
-                <p>This is a native dialog modal</p>
+                <button className="close-button" onClick={() => closeModal()}>X</button>
+                <video muted controls autoPlay loop>
+                    <source src={assetSrc} type="video/webm" />
+                </video>
+                <div className="copy-container">
+                    <h2>Native Modal Dialog</h2>
+                    <p>This is a native dialog modal</p>
+                </div>
             </dialog>
         </div>
     );
